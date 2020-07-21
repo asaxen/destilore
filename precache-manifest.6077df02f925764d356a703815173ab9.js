bda8eb16b7107ad4a3ac701ceb6c5b384f8866e1
adam.saxen <adam.saxen@husqvarnagroup.com>
2020-07-21 12:41:38 +0200

    Updates

---
+++ precache-manifest.6077df02f925764d356a703815173ab9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44dfe0cdf27a291df12779f7fa6ca7ef",
+    "url": "/destilore/index.html"
+  },
+  {
+    "revision": "64967a05f54bb763d549",
+    "url": "/destilore/static/js/2.a3e2712a.chunk.js"
+  },
+  {
+    "revision": "301056a7ca16ead6156b38e6917dbb28",
+    "url": "/destilore/static/js/2.a3e2712a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ba225a41cacb7ee1c7f",
+    "url": "/destilore/static/js/main.4c6aa734.chunk.js"
+  },
+  {
+    "revision": "51c2cb64a00c72fe5633",
+    "url": "/destilore/static/js/runtime-main.796f6ced.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e52fad413d83db1b08d3e0343f989341.js"
+  "/destilore/precache-manifest.6077df02f925764d356a703815173ab9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/destilore/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
